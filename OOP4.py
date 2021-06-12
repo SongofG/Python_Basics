ba8724395f7c9ef10ee6d8e7af4ac5e2fe1f4d00
Haein Park <steve121212@naver.com>
2021-06-12 18:50:47 +0900

    Constructor example

---
+++ OOP4.py
@@ -0,0 +1,17 @@
+## Usage of Constructor
+class AgeInfo:
+    def __init__(self, age):
+        self.age = age
+    def up_age(self):
+        self.age += 1
+    def get_age(self):
+        return self.age
+    def set_age(self, age):
+        self.age = age
+
+def main():
+    fa = AgeInfo(39)
+    fa.up_age()
+    print("1년 후 아빠 나이:", fa.get_age())
+
+main()
